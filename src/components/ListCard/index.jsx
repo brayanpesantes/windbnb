a13ed7b95d4cbeee923d62f7706d4a4032ae07ea
brayanpesantes <brayalexis.30@gmail.com>
2022-05-06 01:28:10 -0500

    create el componet  list card y add data

---
+++ src/components/ListCard/index.jsx
@@ -0,0 +1,38 @@
+import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Card from "../Card";
+
+function ListCard({ guestTotal, locationText }) {
+  const [stays, setStays] = useState([]);
+
+  const getStays = async () => {
+    const data = await fetch("../../../stays.json");
+    return await data.json();
+  };
+  useEffect(() => {
+    getStays().then((item) => setStays(item));
+  }, []);
+
+  useEffect(() => {
+    if (guestTotal !== 0) {
+      getStays().then((item) =>
+        setStays(
+          item.filter((stay) => stay.maxGuests === guestTotal && stay.beds > 0)
+        )
+      );
+    }
+    console.log(guestTotal);
+  }, [guestTotal, locationText]);
+
+  return (
+    <SimpleGrid columns={[2, null, 3]} spacing={"32px"}>
+      {stays.length === 0 ? (
+        <Heading textAlign={"center"}>Ho Hay Data</Heading>
+      ) : (
+        stays?.map((stay, index) => <Card item={stay} key={index} />)
+      )}
+    </SimpleGrid>
+  );
+}
+
+export default ListCard;
