4eff4f82c3a8bf9f4465efe6a1184b03f54f97ef
brayanpesantes <brayalexis.30@gmail.com>
2022-05-06 01:28:26 -0500

    agreacion de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Container, Flex, Heading, HStack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ListCard from "./components/ListCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [locationText, setLocationText] = useState("");
+  const [guestTotal, setGuestTotal] = useState(0);
+
+  const handleGuestTotal = (total) => {
+    console.log(total);
+    setGuestTotal(total);
+  };
+  console.log(guestTotal);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Flex w={"full"} h="100vh" minH={"100vh"} alignContent={"flex-start"}>
+      <Container maxW={"container.lg"}>
+        <Header
+          setLocationText={setLocationText}
+          setGuestTotal={handleGuestTotal}
+        />
+        <HStack justifyContent={"space-between"} align={"center"} mb={"2rem"}>
+          <Heading fontSize={"24px"} lineHeight={"29.26px"} color={"#333333"}>
+            Stay in Finland
+          </Heading>
+          <Text
+            as={"span"}
+            fontSize={"14px"}
+            fontWeight="medium"
+            lineHeight={"17.07px"}
+            color={"#4F4F4F"}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+            12+ stay
+          </Text>
+        </HStack>
+        <ListCard guestTotal={guestTotal} locationText={locationText} />
+        <Footer />
+      </Container>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
